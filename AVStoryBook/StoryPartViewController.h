5efb89a7d2ab75b91134adb2eb913498af33504b
Nick Guerrero <nicolasjguerrero@gmail.com>
2017-09-22 15:44:59 -0400

    Initial Setup Storyboard

---
+++ AVStoryBook/StoryPartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryPartViewController.h
+//  AVStoryBook
+//
+//  Created by Nicolas Guerrero on 9/22/17.
+//  Copyright © 2017 Nicolas Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryPartViewController : UIViewController
+
+@end
---
+++ AVStoryBook/StoryPartViewController.m
@@ -0,0 +1,34 @@
+//
+//  StoryPartViewController.m
+//  AVStoryBook
+//
+//  Created by Nicolas Guerrero on 9/22/17.
+//  Copyright © 2017 Nicolas Guerrero. All rights reserved.
+//
+
+#import "StoryPartViewController.h"
+
+@interface StoryPartViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *cameraButton;
+@property (weak, nonatomic) IBOutlet UIButton *microphoneButton;
+
+@end
+
+@implementation StoryPartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)cameraButtonClicked:(id)sender {
+    
+}
+
+- (IBAction)microphoneButtonClicked:(id)sender {
+    
+}
+
+@end
